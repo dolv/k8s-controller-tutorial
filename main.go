a84c1360fa64534dbcd3bf0168df1815c96089a7
Oleksandr DUDCHENKO <ovdudchenko@gmail.com>
2025-06-22 16:54:45 +0000

    fix module path

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/yourusername/k8s-controller-tutorial/cmd"
+import "github.com/dolv/k8s-controller-tutorial/cmd"
 
 func main() {
 	cmd.Execute()
