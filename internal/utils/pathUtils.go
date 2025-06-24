595f7b70d8f2e7feda1fe1c842a7d727123ced2f
Oleksandr DUDCHENKO <ovdudchenko@gmail.com>
2025-06-24 12:38:14 +0000

    add internal/utils package

---
+++ internal/utils/pathUtils.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func ExpandPath(path string) (string, error) {
+	if path == "~" {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		return home, nil
+	}
+	if strings.HasPrefix(path, "~/") {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return "", err
+		}
+		return filepath.Join(home, path[2:]), nil
+	}
+	return path, nil
+}
